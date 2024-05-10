0f32870c73d170025ccc6140e880700e7cdee957
Claudia Tobias <113573031+Fl4utia@users.noreply.github.com>
2024-05-10 08:40:53 -0600

    Add level 8

--- conversor_imagenes.py
+++ conversor_imagenes.py
@@ -2,21 +2,21 @@ from PIL import Image
 import turtle
 
 # Abre la imagen y la redimensiona
-img1 = Image.open("sumah.gif")
+img1 = Image.open("sumacinco.gif")
 img1_resized = img1.resize((180, 80))  # Cambia el tamaño a 180x80 píxeles
-img1_resized.save("sumah_small.gif")  # Guarda la imagen redimensionada
+img1_resized.save("sumacinco_small.gif")  # Guarda la imagen redimensionada
 
 win = turtle.Screen()
 
 # Registra las imágenes redimensionadas como nuevas formas
-win.addshape("sumah_small.gif")
+win.addshape("sumacinco_small.gif")
 
 # Crea nuevas turtles
 img_turtle1 = turtle.Turtle()
 
 
 # Cambia la forma de las turtles a las imágenes redimensionadas
-img_turtle1.shape("sumah_small.gif")
+img_turtle1.shape("sumacinco_small.gif")
 
 # Mueve las turtles a posiciones específicas si es necesario
 img_turtle1.goto(-100, 0)
